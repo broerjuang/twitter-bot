8802dc77433d89caee7379fd0097c11ee75e856c
broerjuang <juangwiantoro@gmail.com>
2017-05-07 02:38:57 +0700

    restructuring the folder

--- src/config.js
+++ src/config.js
@@ -1,11 +1,16 @@
-import {config} from 'dotenv';
-config();
+// @flow
+require('dotenv').config();
+import createTwitterBot from './helpers/createTwitterBot';
 
-const credentials = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+let credentials = {
+  consumer_key: process.env.CONSUMER_KEY || '',
+  consumer_secret: process.env.CONSUMER_SECRET || '',
+  access_token: process.env.ACCESS_TOKEN || '',
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET || '',
 };
 
-export default credentials;
+let timeout = 60 * 1000;
+
+let bot = createTwitterBot(credentials, timeout);
+
+export default bot;
--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 // @flow
-import credentials from './config';
+import {
+  getAllFollowers,
+  getFollowings,
+  getHomeTimeLine,
+  doRetweet,
+  unRetweet,
+  doLikeTweet,
+  unlikeTweet,
+} from './controller';
+
+
+// Example
+
+getAllFollowers(`broerjuang`)
+  .then(({users}) => users)
+  .then((followers) => followers.map((user) => user.screen_name))
+  .then(console.log);
+
+// it will give us the list of all follower.screen_name;
